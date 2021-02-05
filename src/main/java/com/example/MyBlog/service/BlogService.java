4e0e311206365cef6b7cf0a932392c51ed18126c
wuxiaohanDUT <2658343834@qq.com>
2021-02-05 20:27:11 +0800

    Add Utils

---
+++ src/main/java/com/example/MyBlog/service/BlogService.java
@@ -0,0 +1,10 @@
+package com.example.MyBlog.service;
+
+import com.example.MyBlog.entity.Blog;
+
+public interface BlogService {
+
+    String saveBlog(Blog blog);
+
+
+}
---
+++ src/main/java/com/example/MyBlog/util/MarkDownUtil.java
@@ -0,0 +1,28 @@
+package com.example.MyBlog.util;
+
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+
+public class MarkDownUtil {
+
+    /**
+     * 将md格式转换为html格式
+     */
+    public static String mdToHtml(String markdownString) {
+        if (StringUtils.isEmpty(markdownString)) {
+            return "";
+        }
+        java.util.List<Extension> extensions = Arrays.asList(TablesExtension.create());
+        Parser parser = Parser.builder().extensions(extensions).build();
+        Node document = parser.parse(markdownString);
+        HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).build();
+        String content = renderer.render(document);
+        return content;
+    }
+}
