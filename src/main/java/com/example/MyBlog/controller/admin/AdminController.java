06ce7c733a3c390724990bde15593c05aaae1fc0
wuxiaohanDUT <2658343834@qq.com>
2021-02-14 17:25:55 +0800

    Add AdminController

--- src/main/java/com/example/MyBlog/controller/admin/AdminController.java
+++ src/main/java/com/example/MyBlog/controller/admin/AdminController.java
@@ -2,7 +2,7 @@ package com.example.MyBlog.controller.admin;
 
 
 import com.example.MyBlog.entity.AdminUser;
-import com.example.MyBlog.service.AdminUserService;
+import com.example.MyBlog.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -17,17 +17,159 @@ public class AdminController {
 
     @Resource
     private AdminUserService adminUserService;
+    @Resource
+    private BlogService blogService;
+    @Resource
+    private CategoryService categoryService;
+    @Resource
+    private LinkService linkService;
+    @Resource
+    private TagService tagService;
+    @Resource
+    private CommentService commentService;
 
     @GetMapping("/login")
     public String login(){
         return "admin/login";
     }
 
-    @PostMapping("/login")
+    /**
+     * 处理后台管理界面，获取到关于博客的各自信息
+     * @param request
+     * @return
+     */
+    @GetMapping({"","/","/index","/index.html"})
+    public String index(HttpServletRequest request){
+        request.setAttribute("path","index");
+        //分类数量
+        request.setAttribute("categoryCount",categoryService.getTotalCategories());
+        //总文章数
+        request.setAttribute("blogCount", blogService.getTotalBlogs());
+        //链接数量
+        request.setAttribute("linkCount", linkService.getTotalLinks());
+        //标签总数
+        request.setAttribute("tagCount", tagService.getTotalTags());
+        //收到的评论数
+        request.setAttribute("commentCount", commentService.getTotalComments());
+        return "admin/index";
+    }
+
+    /**
+     * 处理用户登录请求，保存用户登录状态
+     * @param userName
+     * @param password
+     * @param verifyCode
+     * @param session
+     * @return
+     */
+    @PostMapping(value = "/login")
     public String login(@RequestParam("userName") String userName,
                         @RequestParam("password") String password,
                         @RequestParam("verifyCode") String verifyCode,
                         HttpSession session){
-        return "";
+        /*对用户提交的信息进行合法性判断*/
+        if (StringUtils.isEmpty(verifyCode)) {
+            session.setAttribute("errorMsg", "验证码不能为空");
+            return "admin/login";
+        }
+        if (StringUtils.isEmpty(userName) || StringUtils.isEmpty(password)) {
+            session.setAttribute("errorMsg", "用户名或密码不能为空");
+            return "admin/login";
+        }
+        String kaptchaCode = session.getAttribute("verifyCode") + "";
+        if (StringUtils.isEmpty(kaptchaCode) || !verifyCode.equals(kaptchaCode)) {
+            session.setAttribute("errorMsg", "验证码错误");
+            return "admin/login";
+        }
+        AdminUser adminUser = adminUserService.login(userName, password);
+        if (adminUser != null) {
+            session.setAttribute("loginUser", adminUser.getNickName());
+            session.setAttribute("loginUserId", adminUser.getAdminUserId());
+            //session过期时间设置为7200秒 即两小时
+            //session.setMaxInactiveInterval(60 * 60 * 2);
+            return "redirect:/admin/index";
+        } else {
+            session.setAttribute("errorMsg", "登陆失败");
+            return "admin/login";
+        }
+    }
+
+    /**
+     * 处理修改页面的请求，获取用户的信息
+     * @param request
+     * @return
+     */
+    @GetMapping("/profile")
+    public String profile(HttpServletRequest request) {
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        AdminUser adminUser = adminUserService.getUserDetailById(loginUserId);
+        if (adminUser == null) {
+            return "admin/login";
+        }
+        request.setAttribute("path", "profile");
+        request.setAttribute("loginUserName", adminUser.getLoginUserName());
+        request.setAttribute("nickName", adminUser.getNickName());
+        return "admin/profile";
+    }
+
+    /**
+     * 用户修改个人密码，移除session中的登录信息，重新登录
+     * @param request
+     * @param originalPassword
+     * @param newPassword
+     * @return
+     */
+    @PostMapping("/profile/password")
+    @ResponseBody
+    public String passwordUpdate(HttpServletRequest request, @RequestParam("originalPassword") String originalPassword,
+                                 @RequestParam("newPassword") String newPassword) {
+        if (StringUtils.isEmpty(originalPassword) || StringUtils.isEmpty(newPassword)) {
+            return "参数不能为空";
+        }
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        if (adminUserService.updatePassword(loginUserId, originalPassword, newPassword)) {
+            //修改成功后清空session中的数据，前端控制跳转至登录页
+            request.getSession().removeAttribute("loginUserId");
+            request.getSession().removeAttribute("loginUser");
+            request.getSession().removeAttribute("errorMsg");
+            return "success";
+        } else {
+            return "修改失败";
+        }
+    }
+
+    /**
+     * 修改用户姓名
+     * @param request
+     * @param loginUserName
+     * @param nickName
+     * @return
+     */
+    @PostMapping("/profile/name")
+    @ResponseBody
+    public String nameUpdate(HttpServletRequest request, @RequestParam("loginUserName") String loginUserName,
+                             @RequestParam("nickName") String nickName) {
+        if (StringUtils.isEmpty(loginUserName) || StringUtils.isEmpty(nickName)) {
+            return "参数不能为空";
+        }
+        Integer loginUserId = (int) request.getSession().getAttribute("loginUserId");
+        if (adminUserService.updateName(loginUserId, loginUserName, nickName)) {
+            return "success";
+        } else {
+            return "修改失败";
+        }
+    }
+
+    /**
+     * 退出登录
+     * @param request
+     * @return
+     */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("loginUserId");
+        request.getSession().removeAttribute("loginUser");
+        request.getSession().removeAttribute("errorMsg");
+        return "admin/login";
     }
 }
