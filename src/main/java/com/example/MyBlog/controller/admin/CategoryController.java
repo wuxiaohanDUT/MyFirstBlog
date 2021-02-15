41e66dd35b6abf31502945461446164695a0b7e1
wuxiaohanDUT <2658343834@qq.com>
2021-02-15 16:31:40 +0800

    Add CategoryController

---
+++ src/main/java/com/example/MyBlog/controller/admin/CategoryController.java
@@ -0,0 +1,106 @@
+package com.example.MyBlog.controller.admin;
+
+
+import com.example.MyBlog.service.CategoryService;
+import com.example.MyBlog.util.PageQueryUtil;
+import com.example.MyBlog.util.Result;
+import com.example.MyBlog.util.ResultGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/admin")
+public class CategoryController {
+
+    @Resource
+    private CategoryService categoryService;
+
+    /**
+     * 点击分类管理
+     * @param request
+     * @return
+     */
+    @GetMapping("/categories")
+    public String categoryPage(HttpServletRequest request){
+        request.setAttribute("path","categories");
+        return "admin/category";
+    }
+
+    /**
+     * 查询并返回分类列表
+     * @param params
+     * @return
+     */
+    @GetMapping("/categories/list")
+    @ResponseBody
+    public Result list(@RequestParam Map<String,Object> params){
+        if (StringUtils.isEmpty(params.get("page")) || StringUtils.isEmpty(params.get("limit"))) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(categoryService.getBlogCategoryPage(pageUtil));
+    }
+
+    /**
+     * 分类添加
+     */
+    @PostMapping("/categories/save")
+    @ResponseBody
+    public Result save(@RequestParam("categoryName") String categoryName,
+                       @RequestParam("categoryIcon") String categoryIcon) {
+        if (StringUtils.isEmpty(categoryName)) {
+            return ResultGenerator.genFailResult("请输入分类名称！");
+        }
+        if (StringUtils.isEmpty(categoryIcon)) {
+            return ResultGenerator.genFailResult("请选择分类图标！");
+        }
+        if (categoryService.saveCategory(categoryName, categoryIcon)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("分类名称重复");
+        }
+    }
+
+    /**
+     * 分类修改
+     */
+    @PostMapping("/categories/update")
+    @ResponseBody
+    public Result update(@RequestParam("categoryId") Integer categoryId,
+                         @RequestParam("categoryName") String categoryName,
+                         @RequestParam("categoryIcon") String categoryIcon) {
+        if (StringUtils.isEmpty(categoryName)) {
+            return ResultGenerator.genFailResult("请输入分类名称！");
+        }
+        if (StringUtils.isEmpty(categoryIcon)) {
+            return ResultGenerator.genFailResult("请选择分类图标！");
+        }
+        if (categoryService.updateCategory(categoryId, categoryName, categoryIcon)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("分类名称重复");
+        }
+    }
+
+
+    /**
+     * 分类删除
+     */
+    @PostMapping("/categories/delete")
+    @ResponseBody
+    public Result delete(@RequestBody Integer[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        if (categoryService.deleteBatch(ids)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult("删除失败");
+        }
+    }
+}
